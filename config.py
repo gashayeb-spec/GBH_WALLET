ae2b2db552362b8f7c1a383bbb86e27e8925a9b4
gashayeb-spec <gashayeb@gmail.com>
2026-08-25 12:32:22 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,18 @@
+# config.py
+import os
+
+# Telegram Bot Token
+BOT_TOKEN = "8416599811:AAG3WvG-3Pd7hqSUvIMw7r4Gzfg5sz3-MC4"
+
+# Admin Telegram Chat ID
+ADMIN_ID = 5351353727
+
+# Chapa Payment API Keys
+CHAPA_SECRET_KEY = "CHASECK-BEFoKzydvL2HU4AJaKN838jr7sqyq66Q"
+CHAPA_PUBLIC_KEY = "CHAPUBK-St5pcr3rakMbDi1NOZejNZNtcptYVa7W"
+
+# WebApp URL (Render URL)
+BASE_URL = "https://gbh-wallet.onrender.com"
+
+# Master TON Wallet Address for Crypto Deposits
+MASTER_TON_ADDRESS = "EQD________________________________________"
